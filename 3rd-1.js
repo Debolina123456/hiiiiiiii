849540bd879998be43b48054460e4df4086b275f
Debolina123456 <159935452+Debolina123456@users.noreply.github.com>
2024-02-13 23:00:43 +0530

    Add files via upload

---
+++ 3rd-1.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Who is always right?",
+        answer: "C.Ofc baby...u r always right sweetheart...my lil angel...the most beautiful girl i ever saw˖⁺‧₊˚♡˚₊‧⁺˖≽^•⩊•^≼🐻‍❄️ྀིྀི✿",
+        options: [
+            "A.Huh...Me!",
+            "B.You",
+            "C.Ofc baby...u r always right sweetheart...my lil angel...the most beautiful girl i ever saw˖⁺‧₊˚♡˚₊‧⁺˖≽^•⩊•^≼🐻‍❄️ྀིྀི✿"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Who is the most beautiful girl u ever saw?",
+        answer: "A.Obviously...it's u babyyy",
+        options: [
+            "A.Obviously...it's u babyyy",
+            "B.My ex...ofc!",
+            "C.Every other girl except u"
+
+        ]
+    },
+    {
+        numb: 3,
+        question: " Who should say sorry first?",
+        answer: "C. Babyyy...that's not even a question!!!...ofc i will say sorry first...u can never do anything wrong babe!!!",
+        options: [
+           "A. The person who was wrong",
+            "B. No one",
+            "C. Babyyy...that's not even a question!!!...ofc i will say sorry first...u can never do anything wrong babe!!!"
+
+        ]
+    },
+    {
+        numb: 4,
+        question: "Who is ur first love??",
+        answer: "B. My sweetu Mom",
+        options: [
+            "A. My first ex",
+             "B. My sweetu Mom",
+            "C. It's u babe"
+
+
+        ]
+    },
+    {
+        numb: 5,
+        question: "Do i ever fight with u without any reason?",
+        answer: "A. No babe..not at all..ofc it's always my fault that we have to fight",
+        options: [
+            "A. No babe..not at all..ofc it's always my fault that we have to fight",
+            "B. Yeah..sometimes",
+            "C. You are a crazy ass woman who fights for no reason"
+
+        ]
+    }
+];
